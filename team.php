f59e37777ea9a81a0e50a5b78a2f6ec8c7a20367
unknown <bidahal@DWNPCPU010-PC.dwnp.deerwalk.com>
2015-09-04 20:46:11 +0545

    file added - team.php

---
+++ team.php
@@ -0,0 +1,93 @@
+<?php
+require 'init.php';
+require 'controller/ourTeamController.php';
+require 'includes/header.php';
+?>
+
+<div class="content">
+    <div class="infoWrap">
+        <div class="infoContainer" style="width: 790px;">
+            <h3 style="border: none;"><i class="fa fa-user-secret glyphiconOverride"></i> Our Team Members</h3>
+            <?php
+            if (!empty($teamInfo)) {
+                foreach ($teamInfo as $team) {
+                    ?>
+                    <div class="panel panel-default suggestedPnl">
+                        <div class="thumbnail thumbnailOverride">
+                            <!--<div class="teamIcon" style="background-image:url('<?php echo $team['photo'] ?>');"></div>-->
+                            <div class="teamIcon">
+                                <img src="<?php echo $team['photo']; ?>" alt="Team Member" title="Team Member"/>
+                            </div>
+                        </div>
+                        <b class="suggestedPnlTitle"><?php echo ucfirst($team['name']); ?></b>
+                        <br/>
+                        <br/>
+                        <b style="color: #1475A1;">Designation: <?php echo $team['designation']; ?></b> 
+                        <br/>
+                        <br/>
+                        <p><?php echo $team['desc']; ?></p>
+                    </div>
+                    <?php
+                }
+            }
+            ?>
+        </div>
+        <?php require 'includes/reviewBar.php'; ?>
+    </div>
+</div>
+<?php require 'includes/otherInfo.php'; ?>
+<script type="text/javascript">
+    $(function () {
+        (function (e, t, n) {
+            var r, i = e.getElementsByTagName(t)[0];
+            if (e.getElementById(n))
+                return;
+            r = e.createElement(t);
+            r.id = n;
+            r.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+            i.parentNode.insertBefore(r, i)
+        })(document, "script", "facebook-jssdk")
+    });
+
+    $(document).ready(function () {
+        $('#show1').css({display: 'block'});
+        $('#plus1').css({display: 'none'});
+        $('#plus2').css({display: 'block'});
+        $('#plus3').css({display: 'block'});
+        $('#minus1').css({display: 'block'});
+        $('#minus2').css({display: 'none'});
+        $('#minus3').css({display: 'none'});
+        $('#click1').click(function () {
+            $('#show1').slideToggle();
+            $('#show2').slideUp();
+            $('#show3').slideUp();
+            $('#click1 span').toggle();
+            $('#plus2').css({display: 'block'});
+            $('#plus3').css({display: 'block'});
+            $('#minus2').css({display: 'none'});
+            $('#minus3').css({display: 'none'});
+        });
+        $('#click2').click(function () {
+            $('#show2').slideToggle();
+            $('#show1').slideUp();
+            $('#show3').slideUp();
+            $('#click2 span').toggle();
+            $('#plus1').css({display: 'block'});
+            $('#plus3').css({display: 'block'});
+            $('#minus1').css({display: 'none'});
+            $('#minus3').css({display: 'none'});
+        });
+        $('#click3').click(function () {
+            $('#show3').slideToggle();
+            $('#show2').slideUp();
+            $('#show1').slideUp();
+            $('#click3 span').toggle();
+            $('#plus1').css({display: 'block'});
+            $('#plus2').css({display: 'block'});
+            $('#minus1').css({display: 'none'});
+            $('#minus2').css({display: 'none'});
+        });
+    });
+</script>
+<?php
+require 'includes/footer.php';
